b7193233640ff6a68c46b90bb4570cbf5bdf23e3
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-08 23:58:17 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from backend.config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    CORS(app)
+
+    # Importamos aquí para evitar dependencias circulares
+    from backend.routes import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
 
