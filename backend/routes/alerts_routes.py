2b008e6354bfe1e4b479ca447d988dcfffa6f68c
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-28 00:31:49 -0600

    Update alerts_routes.py

--- backend/routes/alerts_routes.py
+++ backend/routes/alerts_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from backend.alerts import enviar_alerta_whatsapp_twilio  # Ajusta si cambia tu estructura
 
-alertas_bp = Blueprint('alertas_bp', __name__)
+alerts_bp = Blueprint('alerts_bp', __name__)
 
-@alertas_bp.route('/api/verificar-uso', methods=['GET'])
+@alerts_bp.route('/api/verificar-uso', methods=['GET'])
 def verificar_uso():
     status = enviar_alerta_whatsapp_twilio()
     return jsonify({"mensaje": "Alerta enviada por WhatsApp", "status": status}), 200
